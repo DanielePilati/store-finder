edd7f6fd9994e96626d51672f5c3f2fa40a8ed6b
DanielePilati <165797370+DanielePilati@users.noreply.github.com>
2024-11-28 16:49:41 +0100

    add index

--- src/main/java/com/finder/store/controller/MainController.java
+++ src/main/java/com/finder/store/controller/MainController.java
@@ -1,16 +1,27 @@
 package com.finder.store.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.finder.store.service.StoreService;
 
 
 @Controller
 @RequestMapping("/")
 public class MainController {
 	
+	@Autowired
+	StoreService storeService;
+	
 	@GetMapping()
-	public String homePage(){		
+	public String homePage(Model model){	
+		
+		//store index
+		model.addAttribute("stores", storeService.getAll());
+		
 		return "home";
 	}
 }
--- src/main/java/com/finder/store/controller/StoreController.java
+++ src/main/java/com/finder/store/controller/StoreController.java
@@ -1,12 +1,27 @@
 package com.finder.store.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.finder.store.service.StoreService;
 
 @Controller
 @RequestMapping("/stores")
 public class StoreController {
 
+	@Autowired
+	StoreService storeService;
 	
+	@GetMapping("/search")
+	public String index(@RequestParam(name = "address") String address, Model model) {
+		
+		model.addAttribute("stores", storeService.getAll());
+		
+		return "home";
+	}
 }
