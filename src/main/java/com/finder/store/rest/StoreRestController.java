d60fbb41a6816efce2f5c715da3d4f0e85244237
DanielePilati <165797370+DanielePilati@users.noreply.github.com>
2024-12-04 13:18:22 +0100

    fix

--- src/main/java/com/finder/store/rest/StoreRestController.java
+++ src/main/java/com/finder/store/rest/StoreRestController.java
@@ -9,8 +9,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.config.TaskExecutionOutcome.Status;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
